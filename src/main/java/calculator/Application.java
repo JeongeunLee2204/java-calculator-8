2509d71f3fa18e7254b4402b9ffcdc587dce76a4
jelee <jelee2204@dgu.ac.kr>
2025-10-19 03:01:58 +0900

    feat(application): add custom delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,12 +6,20 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String s = Console.readLine();
+
         if (s == null || s.isEmpty()) {
             System.out.println("결과 : 0");
             return;
         }
 
-        String[] arr = s.split("[,:]");
+        String d = "[,:]";
+        if (s.startsWith("//")) {
+            int n = s.indexOf("\n");
+            d = s.substring(2, n);
+            s = s.substring(n + 1);
+        }
+
+        String[] arr = s.split(d);
         int sum = 0;
         for (String x : arr) sum += Integer.parseInt(x);
         System.out.println("결과 : " + sum);
