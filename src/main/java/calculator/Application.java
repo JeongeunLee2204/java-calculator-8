57d2b1caf351a9d5b3e8a7a10dfe76202fb9b63b
jelee <jelee2204@dgu.ac.kr>
2025-10-19 02:56:56 +0900

    feat(application): add , and : as delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,14 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String s = Console.readLine();
+        if (s == null || s.isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
+        }
+
+        String[] arr = s.split("[,:]");
+        int sum = 0;
+        for (String x : arr) sum += Integer.parseInt(x);
+        System.out.println("결과 : " + sum);
     }
 }
